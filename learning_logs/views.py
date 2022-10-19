98dab49146a8f0b09bcb197a3e81419b74d1832c
Myllena Tayná <myllenatayna21@gmail.com>
2022-10-18 21:20:15 -0300

    Adicionando novasentradas

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,10 +1,8 @@
-from multiprocessing import context
-from urllib import request
 from django.shortcuts import render
-from .models import Topic
+from .models import Topic, Entry
 from django.http import HttpResponseRedirect
 from django.urls import reverse
-from .forms import TopicForm
+from .forms import TopicForm, EntryForm
 
 def index(request):
     return render(request, 'learning_logs/index.html')
@@ -30,3 +28,31 @@ def new_topic(request):
         return HttpResponseRedirect(reverse('topics'))
     context = {'form': form}
     return render(request, 'learning_logs/new_topic.html', context)
+
+def new_entry(request,topic_id):
+    topic = Topic.objects.get(id=topic_id)
+    if request.method != 'POST':
+        form = EntryForm()
+    else:
+        form = EntryForm(data=request.POST)
+    if form.is_valid():
+        new_entry = form.save(commit=False)
+        new_entry.topic = topic
+        new_entry.save()
+        return HttpResponseRedirect(reverse('topic', args=[topic_id]))
+    context = {'topic': topic, 'form': form}
+    return render(request,'learning_logs/new_entry.html', context)
+
+def edit_entry(request,entry_id):
+    entry = Entry.objects.get(id=entry_id)
+    topic = entry.topic
+    if request.method != 'POST':
+        form = EntryForm(instance=entry)
+    else:
+        form = EntryForm(instance=entry, data=request.POST)
+    if form.is_valid():
+        form.save()
+        return HttpResponseRedirect(reverse('topic', args=[topic.id]))
+    context = {'entry': entry, 'topic': topic, 'form': form}
+    return render(request, 'learning_logs/edit_entry.html', context)
+
