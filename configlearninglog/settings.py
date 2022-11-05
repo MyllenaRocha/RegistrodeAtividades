1060d139b65ba42b2bb9559f6dbfb7acbdc81fb4
Myllena Tayná <myllenatayna21@gmail.com>
2022-11-05 09:03:41 -0300

    oi

--- configlearninglog/settings.py
+++ configlearninglog/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 
+import bootstrap3
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -39,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'learning_logs',
     'users',
+    'bootstrap3',
 ]
 
 MIDDLEWARE = [
@@ -119,7 +122,17 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Minhas configurações
+LOGIN_URL = '/users/login/'
+
+# configurações para django-bootstrap3
+BOOTSTRAP3 = {
+    'include_jquery': True,
+}
+
